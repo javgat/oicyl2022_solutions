48c9a93bd82c52634269492b0af9425b0702d288
DvRodri8 <diego-break-@hotmail.com>
2022-02-02 17:39:11 +0100

    some input generators

---
+++ f2/input_generator.py
@@ -0,0 +1,8 @@
+from lib2to3.pgen2 import literals
+from random import randint
+
+num_vehicles = randint(1, 10e5)
+for i in range(num_vehicles):
+    kms = randint(50, 50000)
+    litres = randint(3, 20)
+    print(kms, litres)
