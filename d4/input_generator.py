e662edd54ce776ec560cb59f6433a60ea8da7830
Javier Gatón Herguedas <javigaton@gmail.com>
2022-02-04 13:59:50 +0100

    generated inputs d4 and outputs with diego

---
+++ d4/input_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from random import randint
+
+cap_camion = randint(1,1000000)
+print(cap_camion)
+n_paqs = randint(1,10000)
+print(n_paqs)
+for i in range(n_paqs):
+    peso = randint(1,10000)
+    valor = randint(1,10000)
+    print(peso, valor)
