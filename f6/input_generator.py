af0e0a26f9111c633c671271b9c995893089ca93
Javier Gatón Herguedas <javigaton@gmail.com>
2022-02-04 10:32:14 +0100

    f6 input generator updated

--- f6/input_generator.py
+++ f6/input_generator.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 from random import randint
 
 num_pedidos = randint(1, 10e4)
 pedidos = []
 for i in range(num_pedidos):
-    pedidos.append(randint(0, 10000))
+    pedidos.append(randint(0, 99999))
 
 print(*pedidos, sep=',')
